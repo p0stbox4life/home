58f75f21f567b40850e82e429b9c2840d36f12c9
Project Luna <130220883+xplunaproject@users.noreply.github.com>
2023-04-08 15:12:04 +0100

    Update script so no .php is used.

--- script.js
+++ script.js
@@ -1,48 +1,61 @@
-$(document).ready(function() {
+document.addEventListener("DOMContentLoaded", function() {
     loadReviews();
+    document.getElementById("review-form").addEventListener("submit", submitReview);
 });
 
 function loadReviews() {
-    var reviews = JSON.parse(localStorage.getItem("reviews")) || [];
-    reviews.sort(function(a, b) {
-        return b.timestamp - a.timestamp;
-    });
-    var reviewsList = document.getElementById("reviews-list");
-    reviewsList.innerHTML = "";
-    reviews.forEach(function(review) {
-        var li = document.createElement("li");
-        var reviewHTML = '<div class="review-info"><span class="review-name">' + review.name + '</span><span class="review-timestamp">' + formatDate(review.timestamp) + '</span></div><div class="review-text">' + review.review + '</div><div class="review-patches">' + review.patches + '</div>';
-        li.innerHTML = reviewHTML;
-        reviewsList.appendChild(li);
-    });
+    fetch("reviews.txt")
+        .then(response => response.json())
+        .then(reviews => {
+            reviews.sort((a, b) => b.timestamp - a.timestamp);
+            const reviewsList = document.getElementById("reviews-list");
+            reviewsList.innerHTML = "";
+            reviews.forEach(review => {
+                const li = document.createElement("li");
+                const reviewHTML = `
+                    <div class="review-info">
+                        <span class="review-name">${review.name}</span>
+                        <span class="review-timestamp">${formatDate(review.timestamp)}</span>
+                    </div>
+                    <div class="review-text">${review.review}</div>
+                    <div class="review-patches">${review.patches}</div>
+                `;
+                li.innerHTML = reviewHTML;
+                reviewsList.appendChild(li);
+            });
+        })
+        .catch(error => console.error(error));
 }
 
-function submitReview() {
-    var name = document.getElementById("name").value;
-    var review = document.getElementById("review").value;
-    var patches = document.getElementById("patches").value;
-    var timestamp = Date.now() / 1000;
-    var reviews = JSON.parse(localStorage.getItem("reviews")) || [];
-    reviews.push({ name: name, review: review, patches: patches, timestamp: timestamp });
-    localStorage.setItem("reviews", JSON.stringify(reviews));
-    loadReviews();
-    document.getElementById("name").value = "";
-    document.getElementById("review").value = "";
-    document.getElementById("patches").value = "";
+function submitReview(event) {
+    event.preventDefault();
+    const name = document.getElementById("name").value;
+    const review = document.getElementById("review").value;
+    const patches = document.getElementById("patches").value;
+    const timestamp = Math.floor(Date.now() / 1000);
+    const formData = new FormData();
+    formData.append("name", name);
+    formData.append("review", review);
+    formData.append("patches", patches);
+    formData.append("timestamp", timestamp);
+    fetch("submit-review.php", { method: "POST", body: formData })
+        .then(() => {
+            loadReviews();
+            document.getElementById("review-form").reset();
+        })
+        .catch(error => console.error(error));
 }
 
 function formatDate(timestamp) {
-    var date = new Date(timestamp * 1000);
-    var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-    var month = monthNames[date.getMonth()];
-    var day = date.getDate();
-    var year = date.getFullYear();
-    var hour = date.getHours();
-    var minutes = date.getMinutes();
-    var ampm = hour >= 12 ? 'pm' : 'am';
-    hour = hour % 12;
-    hour = hour ? hour : 12;
+    const date = new Date(timestamp * 1000);
+    const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    const month = monthNames[date.getMonth()];
+    const day = date.getDate();
+    const year = date.getFullYear();
+    let hour = date.getHours();
+    let minutes = date.getMinutes();
+    const ampm = hour >= 12 ? 'pm' : 'am';
+    hour = hour % 12 || 12;
     minutes = minutes < 10 ? '0' + minutes : minutes;
-    var strTime = hour + ':' + minutes + ' ' + ampm;
-    return month + ' ' + day + ', ' + year + ' at ' + strTime;
+    return `${month} ${day}, ${year} at ${hour}:${minutes}${ampm}`;
 }
