cabc2b0ea3ccbd4066be8b206f41f117b427cf70
Project Luna <130220883+xplunaproject@users.noreply.github.com>
2023-04-08 15:13:48 +0100

    Delete submit-review.php

--- submit-review.php
+++
@@ -1,8 +0,0 @@
-<?php
-  $name = $_POST["name"];
-  $review = $_POST["review"];
-  $patches = $_POST["patches"];
-  $file = fopen("reviews.txt", "a");
-  fwrite($file, $name . ": " . $review . " [patches: " . $patches . "]\n");
-  fclose($file);
-?>
